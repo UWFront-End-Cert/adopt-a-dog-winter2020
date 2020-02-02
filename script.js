f3b4f5b1cac4538c2daf94dc582e8615c33b3c5c
Jenny Sugden <jensugden@gmail.com>
2020-02-01 17:27:54 -0800

    added javascript file

---
+++ script.js
@@ -0,0 +1,66 @@
+function Angus() {
+  text = 'My name is Angus! I am a 2-year old male Boxer. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Duffy() {
+  text = 'My name is Duffy! I am a 3-year old female Pitbull. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Jack() {
+  text = 'My name is Jack! I am a 6-month old male Beagle. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Jake() {
+  text = 'My name is Jake! I am a 2-year old male Yellow Lab. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Lucas() {
+  text = 'My name is Lucas! I am a 1-year old male German Shepherd mix. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Luna() {
+  text = 'My name is Luna! I am a 5-year old female Yellow Lab. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Maximus() {
+  text = 'My name is Maximus! I am a 4-year old male Schnauzer. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Murphy() {
+  text = 'My name is Murphy! I am a 3-year old male Boxer mix. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Piper() {
+  text = 'My name is Piper! I am a 3-year old female Chocolate Retriever mix. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Poppy() {
+  text = 'My name is Poppy! I am a 2-year old female Shitzu. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Stella() {
+  text = 'My name is Stella! I am a 4-year old female Chihuahua. My adoption fee is $123.45';
+  alert(text);
+}
+
+function Violet() {
+  text = 'My name is Violet! I am a 4-month old female Chocolate Lab. My adoption fee is $123.45';
+  alert(text);
+}
+
+let adoptFee = 123.45;
+
+function dogTotal() {
+  adoptFee = 123.45;
+  alert('$' + adoptFee);
+}
