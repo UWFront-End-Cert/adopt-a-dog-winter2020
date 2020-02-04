eb9ee681e45741e9a9d3fef5665864dd478b893e
kcbelford <kcbelford@hotmail.com>
2020-02-04 13:25:20 -0800

    fixed some errors, added scripts folder, added click handlers to dog photos

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function murphyInfo() {
+  alert('Name: Murphy\nAdoption Fees: $123.45');
+}
+
+function poppyInfo() {
+  alert('Name: Poppy\nAdoption Fees: $123.45');
+}
+
+function jackInfo() {
+  alert('Name: Jack\nAdoption Fees: $123.45');
+}
+
+function duffyInfo() {
+  alert('Name: Duffy\nAdoption Fees: $123.45');
+}
+
+function lucasInfo() {
+  alert('Name: Lucas\nAdoption Fees: $123.45');
+}
+
+function jakeInfo() {
+  alert('Name: Jake\nAdoption Fees: $123.45');
+}
+
+function angusInfo() {
+  alert('Name: Angus\nAdoption Fees: $123.45');
+}
+
+function violetInfo() {
+  alert('Name: Violet\nAdoption Fees: $123.45');
+}
+
+function piperInfo() {
+  alert('Name: Piper\nAdoption Fees: $123.45');
+}
+
+function maximusInfo() {
+  alert('Name: Maximus\nAdoption Fees: $123.34');
+}
+
+function lunaInfo() {
+  alert('Name: Luna\nAdoption Fees: $123.45');
+}
+
+function stellaInfo() {
+  alert('Name: Stella\nAdoption Fees: $123.45');
+}
