265f8ce156c4b3be17b14ac16407dc2654f001f6
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-02-05 22:39:05 -0800

    connect js to website, added button to dogs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adopt-a-dog-winter2020
+// connected javascript to the website
+// added button
+// working on function so I can create a function cart
---
+++ adopt-a-dog.js
@@ -0,0 +1,8 @@
+const cart = [];
+
+const item = {name:"pet", cost:123.45, count:1};
+
+cart.push(item);
+
+console.log(cart);
+console.log(cart[0].name);
