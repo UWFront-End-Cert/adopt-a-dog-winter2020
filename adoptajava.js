cd4c80b41202331b8a898c87262c73c4e1d2b4ad
JennH <marlowmayfield@hotmail.com>
2020-02-05 20:19:39 -0800

    added js file and updated index and show-dogs files

---
+++ adoptajava.js
@@ -0,0 +1,8 @@
+function totalAdder() {
+  let input = prompt('enter price here');
+  let priceToAdd = 100;
+  let result = 0;
+
+  result = priceToAdd + Number(input);
+  alert('Welcome to your new pet! Your total is now' + '$' + result);
+}
