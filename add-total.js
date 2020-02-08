ce4cba2eb3d1ab05f22f7d4ad5a64d9c75001845
Taylor Sommers <taylorsommers@Taylors-MacBook-Pro.local>
2020-02-08 13:29:34 -0800

    updated JavaScript with more elegant method and added info into HTML to pass through functions.

--- add-total.js
+++ add-total.js
@@ -2,9 +2,31 @@
 //Create click handlers on each dog’s button that will add to the total.
 //Alert this total.
 
-let total = 0;
+/*let total = 0;
 
 function addToTotal () {
   total = Number((total + 123.45).toFixed(2));
 
+}
+
+*/
+
+//html
+/*
+<div class="box" onclick="handleClick('Murphy')">
+  <button onclick ="handleCounter(1)">click</button>
+</div>
+
+<div class="box" onclick="handleClick('Murphy')">
+  <button onclick ="handleCounter(23)">click</button>
+</div>
+
+*/
+//JS
+
+let counter = 0;
+
+function handleCounter() {
+  counter++;
+  alert(counter);
 }
--- dog-info.js
+++ dog-info.js
@@ -1,9 +1,26 @@
 
 //Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
 //You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
-
+/*
 function displayDogInfo(name, breed, adoptFee) {
   let result = null;
   result = `You clicked on ${name}. Here's some information: ${breed}${`/n`}Cost to Adopt: ${adoptFee}`;
   alert(result);
 }
+*/
+//JS
+
+function handleClick(dogName, breed, adoptFee) {
+  let result = null;
+  result = `You clicked on ${dogName}. They are a ${breed} and cost $${adoptFee}`;
+  alert(result);
+}
+
+
+
+//HTML
+/*
+<div class="box" onclick="handleClick('Murphy')">1</div>
+<div class="box" onclick="handleClick('Fido')">1</div>
+<div class="box" onclick="handleClick('Bullet')">1</div>
+*/
