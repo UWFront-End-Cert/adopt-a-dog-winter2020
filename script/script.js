59586d710a19a52f5b5e6dc10270f6aeac0045a2
Jennifer Lee <lee.jennifer5221@gmail.com>
2020-02-09 07:00:04 -0800

    updated assignment 4 with more efficient code

--- script/script.js
+++ script/script.js
@@ -1,115 +1,17 @@
+//Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
+
+function dogInfo(name, breed, cost) {
+  result = 'Name: ' + name + '\n' + 'Breed: ' + breed + '\n' + 'Cost: ' + cost;
+  alert(result);
+  //alert('Name: (name)\nBreed: (breed)\nCost: (cost)');
+}
+
 //Create click handlers on each dog’s button that will add to the total.
 //Alert this total.
-function murphyAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Murphy has been added to your cart.\nTotal: ' + '$' + result);
-}
 
-function poppyAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Poppy has been added to your cart.\nTotal: ' + '$' + result);
-}
+let total = 0;
 
-function jackAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  let result = 123.45 * Number(input);
-  alert('Jack has been added to your cart.\nTotal: ' + '$' + result);
-}
-
-function duffyAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  let result = 123.45 * Number(input);
-  alert('Duffy has been added to your cart.\nTotal: ' + '$' + result);
-}
-
-function lucasAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  let result = 123.45 * Number(input);
-  alert('Lucas has been added to your cart.\nTotal: ' + '$' + result);
-}
-function jakeAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  let result = 123.45 * Number(input);
-  alert('Jake has been added to your cart.\nTotal: ' + '$' + result);
-}
-function angusAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Angus has been added to your cart.\nTotal: ' + '$' + result);
-}
-function violetAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Violet has been added to your cart.\nTotal: ' + '$' + result);
-}
-function piperAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Violet has been added to your cart.\nTotal: ' + '$' + result);
-}
-function maximusAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Violet has been added to your cart.\nTotal: ' + '$' + result);
-}
-function lunaAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Luna has been added to your cart.\nTotal: ' + '$' + result);
-}
-function stellaAdded() {
-  let input = prompt('How many dogs are in your cart?');
-  result = 123.45 * Number(input);
-  alert('Stella has been added to your cart.\nTotal: ' + '$' + result);
-}
-
-/*(function() {
-  let adoptButtons = document.getElementsByClassName('adopt');
-  for (let i = 0; i < adoptButtons.length; i++);
-    let button = adoptButtons[i];
-    button.addEventListener('click', function(event) {
-      let buttonClicked = event.target;
-      buttonClicked.parentElement.parentElement.add
-})()*/
-
-
-//Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
-//You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
-function murphy() {
-  alert('Name: Murphy\nAdoption Cost: $123.45\nBreed: Boxer');
-}
-function poppy() {
-  alert('Name: Poppy\nAdoption Cost: $123.45\nBreed: Lhasa Apso');
-}
-function jack() {
-  alert('Name: Jack\nAdoption Cost: $123.45\nBreed: Beagles')
-}
-function duffy() {
-  alert('Name: Duffy\nAdoption Cost: $123.45\nBreed: Basenji')
-}
-function lucas() {
-  alert('Name: Lucas\nCost: $123.45\nBreed: Rottweiler')
-}
-function jake() {
-  alert('Name: Jake\nCost: $123.45\nBreed: Labrador')
-}
-function angus() {
-  alert('Name: Angus\nCost: $123.45\nBreed: Pit Bull')
-}
-function violet() {
-  alert('Name: Violet\nCost: $123.45\nBreed: Labrador')
-}
-function piper() {
-  alert('Name: Piper\nCost: $123.45\nBreed: Golden Retriever')
-}
-function maximus() {
-  alert('Name: Maximus\nCost: $123.45\nBreed: Scottish Terrier')
-}
-function luna() {
-  alert('Name: Luna\nCost: $123.45\nBreed: Labrador')
-}
-function stella() {
-  alert('Name: Stella\nCost: $123.45\nBreed: Chihuahua')
+function addValue(num) {
+  total += num;
+  alert(`$` + total);
 }
