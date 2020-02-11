9db6f10a276bc50adbc6d96a893a7fdd41be80a0
alanna <alannacwhitaker@gmail.com>
2020-02-10 18:19:17 -0800

    basic javascript

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log('Test Test');
+
+
+
+function handleClick(dogName) {
+  alert(dogName); //can also use alert()
+}
+
+
+let counter = 0;
+
+function handleCounter(num) {
+  counter = counter + num;
+  alert(counter); //can also use alert()
+}
