5a0c08deb7efcaf1c7d040b17de3c14da2917ff1
kcbelford <kcbelford@hotmail.com>
2020-02-11 14:38:17 -0800

    made js functions more efficient

--- scripts/script.js
+++ scripts/script.js
@@ -1,111 +1,14 @@
 /*dog pictures - alerts dog info*/
-function murphyInfo() {
-  alert('Name: Murphy\nAdoption Fees: $111.11');
-}
 
-function poppyInfo() {
-  alert('Name: Poppy\nAdoption Fees: $222.22');
-}
-
-function jackInfo() {
-  alert('Name: Jack\nAdoption Fees: $333.33');
-}
-
-function duffyInfo() {
-  alert('Name: Duffy\nAdoption Fees: $444.44');
-}
-
-function lucasInfo() {
-  alert('Name: Lucas\nAdoption Fees: $555.55');
-}
-
-function jakeInfo() {
-  alert('Name: Jake\nAdoption Fees: $666.66');
-}
-
-function angusInfo() {
-  alert('Name: Angus\nAdoption Fees: $777.77');
-}
-
-function violetInfo() {
-  alert('Name: Violet\nAdoption Fees: $888.88');
-}
-
-function piperInfo() {
-  alert('Name: Piper\nAdoption Fees: $999.99');
-}
-
-function maximusInfo() {
-  alert('Name: Maximus\nAdoption Fees: $1000.00');
-}
-
-function lunaInfo() {
-  alert('Name: Luna\nAdoption Fees: $2000.00');
-}
-
-function stellaInfo() {
-  alert('Name: Stella\nAdoption Fees: $3000.00');
+function dogInfo(nameFees) {
+  alert(nameFees);
 }
 
 /*adopt button - alerts total cost*/
+
 let totalCost = 0;
 
-function murphyCost() {
-  totalCost += 11111;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function poppyCost() {
-  totalCost += 22222;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function jackCost() {
-  totalCost += 33333;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function duffyCost() {
-  totalCost += 44444;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function lucasCost() {
-  totalCost += 55555;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function jakeCost() {
-  totalCost += 66666;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function angusCost() {
-  totalCost += 77777;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function violetCost() {
-  totalCost += 88888;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function piperCost() {
-  totalCost += 99999;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function maximusCost() {
-  totalCost += 100000;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function lunaCost() {
-  totalCost += 200000;
-  alert('Total Fees: ' + '$' + totalCost / 100);
-}
-
-function stellaCost() {
-  totalCost += 300000;
+function dogFees(num) {
+  totalCost += num;
   alert('Total Fees: ' + '$' + totalCost / 100);
 }
