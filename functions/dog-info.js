3db6189e9899601f30c0d5b4e7ac281968e54f5c
Christy Lam <cyyllam@gmail.com>
2020-02-19 15:44:22 -0800

    combined js functions into one script

--- functions/dog-info.js
+++ functions/dog-info.js
@@ -1,4 +1,19 @@
 /*
+Create click handlers on each dog’s button that will add to the total.
+Alert this total.
+*/
+
+let total = 0;
+
+function addToTotal() {
+    total = Number((total + 123.45).toFixed(2)); 
+    //a running total of all the dogs somehow produce many decimal places...
+    totalDisplay = total.toFixed(2); 
+    alert(`Your total cost is: $${totalDisplay}`);
+    return total;
+}
+
+/*
 Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
 Pass in the params in the click handler of the dog’s name, breed and adoption fees
 */
