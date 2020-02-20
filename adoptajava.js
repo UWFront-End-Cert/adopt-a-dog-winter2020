43dfd4f49473fcc0be3b29b3cc38ab703a718bd1
JennH <marlowmayfield@hotmail.com>
2020-02-19 23:28:14 -0800

    added resposive features and updates

--- adoptajava.js
+++ adoptajava.js
@@ -1,8 +1,146 @@
-function totalAdder() {
-  let input = prompt('enter price here');
-  let priceToAdd = 100;
+//adoptadog JS page
+function MurphyInfo() {
+  let input = alert('This is Murphy. He is a Terrier. He is high maintenance and high energy. His adoption fee is $123.00')
+};
+
+function PoppyInfo() {
+  let input = alert('This is Poppy. She is a white mix mutt. She is low maintenance and low energy. Her adoption fee is $135.00')
+};
+
+function JackInfo() {
+  let input = alert('This is Jack. He is a Beagel. He is high maintenance and high energy. Her adoption fee is $193.00')
+};
+
+function DuffyInfo() {
+  let input = alert('This is Duffy. She is a Pitbull. She is high maintenance, but low energy. Her adoption fee is $123.00')
+};
+
+function LucasInfo() {
+  let input = alert('This is Lucas. He is a German Shepard. He is low maintenance, but high energy. His adoption fee is $233.00')
+};
+
+function JakeInfo() {
+  let input = alert('This is Jake. He is a Golden Lab. He is low maintenance and low energy. His adoption fee is $195.00')
+};
+
+function AngusInfo() {
+  let input = alert('This is Angus. He is a Boxer Terrier. He is low maintenance, but high energy. His adoption fee is $245.00')
+};
+
+function VioletInfo() {
+  let input = alert('This is Violet. She is a Chocolate Lab Puppy, 2 months old with all of her shots. She is low maintenance, but mid-range energy. She will cost $123.00 to adopt.')
+};
+
+function PiperInfo() {
+  let input = alert('This is Piper. He is a Golden Retriver. He is low maintenance and low energy. His adoption fee is $195.00')
+};
+
+function MaximusInfo() {
+  let input = alert('This is Maximus. He is a mix mutt Scottie. He is low maintenance, but high energy. His adoption fee is $123.00')
+};
+
+function LunaInfo() {
+  let input = alert('This is Luna. She is a Golden Labrador. She is low maintenance and low energy. Her adoption fee is $123.00')
+};
+
+function StellaInfo() {
+  let input = alert('This is Stella. She is a Chihauhua. She is high maintenance and high energy. Her adoption fee is $135.00')
+};
+
+
+function poppyAdder() {
+  let poppyPrice = 135;
   let result = 0;
 
-  result = priceToAdd + Number(input);
-  alert('Welcome to your new pet! Your total is now' + '$' + result);
+  result = poppyPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function MurphyAdder() {
+  let murphyPrice = 123;
+  let result = 0;
+
+  result = murphyPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+
+function jackAdder() {
+  let jackPrice = 193;
+  let result = 0;
+
+  result = jackPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function duffyAdder() {
+  let duffyPrice = 123;
+  let result = 0;
+
+  result = duffyPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function lucasAdder() {
+  let lucasPrice = 233;
+  let result = 0;
+
+  result = lucasPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function jakeAdder() {
+  let jakePrice = 195;
+  let result = 0;
+
+  result = jakePrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function angusAdder() {
+  let angusPrice = 245;
+  let result = 0;
+
+  result = angusPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function violetAdder() {
+  let violetPrice = 123;
+  let result = 0;
+
+  result = violetPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
 }
+
+function piperAdder() {
+  let piperPrice = 195;
+  let result = 0;
+
+  result = piperPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function maximusAdder() {
+  let maximusPrice = 123;
+  let result = 0;
+
+  result = maximusPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function lunaAdder() {
+  let lunaPrice = 123;
+  let result = 0;
+
+  result = lunaPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
+
+function stellaAdder() {
+  let stellaPrice = 135;
+  let result = 0;
+
+  result = stellaPrice + 100;
+  let input = alert('Welcome to your new pet! Your total is now' + '$' + result);
+};
