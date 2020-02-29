c371430a6ca207f7c6fd256aa5ac016efb82e31c
kcbelford <kcbelford@hotmail.com>
2020-02-29 14:52:20 -0800

    started working on lesson8 assignment

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,17 @@ function dogFees(num) {
   totalCost += num;
   alert('Total Fees: ' + '$' + totalCost / 100);
 }
+
+// alerts user form has been submitted
+
+function submitForm() {
+  alert('The form information has been received. There is no turning back now.');
+}
+
+
+// HELP! I'm just trying to add an article element to the div with the class name 'blog-main'. What am I missing?
+
+const blogContainer = document.getElementsByClassName('blog-main');
+const article = document.createElement('article');
+
+blogContainer.appendChild(article);
