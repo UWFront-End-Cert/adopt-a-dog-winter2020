a160a84627fd60b4590ea82ef54da9f2f4d30086
LucasCheadle <56379768+lucascheadle@users.noreply.github.com>
2020-03-04 00:32:43 -0800

    lesson08 with tweaks

--- adoptadog-javascript.js
+++
@@ -1,6 +0,0 @@
-function addToCart() {
-    let total = 123.45;
-    for (i=1; i<=total; i++)
-    alert(total * i); 
-}
-
--- js/blog-javascript.js
+++ js/blog-javascript.js
@@ -1,20 +1,20 @@
 
 const blogContent = [
     {
-        blogPicture: './images/blog-1.jpg',
         blogHeadline: 'Traveling With Your Dog',
+        blogPicture: './images/blog-1.jpg',
         blogText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id, sapiente deleniti impedit laudantium facilis nobis tenetur animi aliquid adipisci fugit provident fuga possimus quam quisquam ab magni dolore aliquam.'
     },
 
     {
-        blogPicture: './images/blog-2.jpg',
         blogHeadline: 'How to Walk Multiple Dogs',
+        blogPicture: './images/blog-2.jpg',
         blogText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id, sapiente deleniti impedit laudantium facilis nobis tenetur animi aliquid adipisci fugit provident fuga possimus quam quisquam ab magni dolore aliquam.'
     },
 
     {
-        blogPicture: './images/blog-3.jpg',
         blogHeadline: 'Teach Your Dog to Fetch',
+        blogPicture: './images/blog-3.jpg',
         blogText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id, sapiente deleniti impedit laudantium facilis nobis tenetur animi aliquid adipisci fugit provident fuga possimus quam quisquam ab magni dolore aliquam.'
     },
 ];
@@ -25,8 +25,8 @@ blogContent.forEach(function(dogBlog) {
     const box = document.createElement('div');
     box.setAttribute('class', 'box');
     box.setAttribute('id', 'firstBox')
-    box.innerHTML += `<img src="${dogBlog.blogPicture}">`
     box.innerHTML += '<h2>' + dogBlog.blogHeadline + '</h2>';
+    box.innerHTML += `<img src="${dogBlog.blogPicture}">`
     box.innerHTML += '<p>' + dogBlog.blogText + '</p>';
 
     target.appendChild(box);
