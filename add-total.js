fb84eaddff608bc18bc4349f2193f67eca242122
Taylor Sommers <taylorsommers@Taylors-MBP.domain>
2020-03-04 17:11:42 -0800

    Added JS for Blog Page Refactor, and updated Checkout pg Submit Button. Lesson 08

--- add-total.js
+++ add-total.js
@@ -1,28 +1,6 @@
-//Link JavaScript to your site.
-//Create click handlers on each dog’s button that will add to the total.
-//Alert this total.
 
-/*let total = 0;
+// Add total Section
 
-function addToTotal () {
-  total = Number((total + 123.45).toFixed(2));
-
-}
-
-*/
-
-//html
-/*
-<div class="box" onclick="handleClick('Murphy')">
-  <button onclick ="handleCounter(1)">click</button>
-</div>
-
-<div class="box" onclick="handleClick('Murphy')">
-  <button onclick ="handleCounter(23)">click</button>
-</div>
-
-*/
-//JS
 
 let totalCost = 0;
 let counter = 0;
@@ -32,3 +10,66 @@ function handleCounter(cost) {
   counter++;
   alert(`You have selected ${counter} dog(s) to adopt. Your total price is $${totalCost}`);
 }
+
+// Dog-Info-Image Section
+
+function handleClick(dogName, breed, adoptFee) {
+  let result = null;
+  result = `You clicked on ${dogName}. They are a ${breed} and cost $${adoptFee}`;
+  alert(result);
+}
+
+
+// Checkout Submit Section
+// On the form page, display an alert that says "Thank you.
+// The form information has been received" when the form has been submitted.
+// Console log the values inside the form, you can build a string or build an object.
+
+
+function checkoutSubmit() {
+  let result2 = null;
+  result2 = 'Thank you!';
+  alert(result2);
+}
+//
+// const submitButton = document.createElement('button');
+// submitButton.setAttribute('type', 'submit');
+// submitButton.textContent = 'submit';
+//
+// form.appendChild(submitButton)
+//
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   et result2 = null;
+//   result = 'Thank you!';
+//     event.preventDefault();
+//     alert (result2);
+//
+// })
+//
+//
+
+
+// const submitButton = document.createElement('button');
+// submitButton.setAttribute('type', 'submit');
+// submitButton.textContent = 'Submit';
+//
+// form.appendChild(submitButton);
+//
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault(); //stops form from refreshing the page.
+//   event.target.elements.forEach(function(element) {
+//
+// for (let i = 0; i < event.target.elements.length - 1; i++) {
+//   console.log(event.target.elements[i].value);
+// }
+//   // console.log(event.target.elements[0].value);
+//   console.log('form submitted');
+//
+// // do things on submit
+// });
+//
+// // unpacking the event
+// Look for "Value" field
+//
+// bodyToChange.appendChild(form);
--- dog-info.js
+++ dog-info.js
@@ -1,26 +1,77 @@
 
-//Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
-//You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
-/*
-function displayDogInfo(name, breed, adoptFee) {
-  let result = null;
-  result = `You clicked on ${name}. Here's some information: ${breed}${`/n`}Cost to Adopt: ${adoptFee}`;
-  alert(result);
+  window.onload = function() {
+    console.log('Page loaded');
+
+  const blogContainer = document.getElementsByClassName('blog-container')[0];
+  const blogHeader = document.createElement('h1');
+
+  blogHeader.textContent = 'Adopt a blog';
+  blogContainer.appendChild(blogHeader);
+
+  const blogParagraphText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+  blogContainerItems = [{blockImage: 'images/blog-1.jpg',
+            					   blockText: {h2: 'travel...',
+                                     p: blogParagraphText}},
+            					  {blockImage: 'images/blog-2.jpg',
+            					   blockText: {h2: 'travel more...',
+                                     p: blogParagraphText}},
+            					  {blockImage: 'images/blog-3.jpg',
+            					   blockText: {h2: 'stay home cuz corano V dawg',
+                                     p: blogParagraphText}}]
+
+  for (let i = 0; i < blogContainerItems.length; i +=1) {
+
+    blockItem = blogContainerItems[i];
+
+    const blogBlock = document.createElement('div');
+      blogBlock.setAttribute('class', 'blog-block');
+      blogContainer.appendChild(blogBlock);
+
+    const blogImage = document.createElement('div');
+      blogImage.setAttribute('class', 'blog-image');
+      blogBlock.appendChild(blogImage);
+
+    const blogRealImage = document.createElement('img');
+      blogRealImage.src = blockItem.blockImage;
+      blogImage.appendChild(blogRealImage);
+
+    const blogText = document.createElement('div');
+      blogText.setAttribute('class', 'blog-text');
+      blogBlock.appendChild(blogText);
+
+    const blogH2 = document.createElement('h2');
+      blogH2.textContent = blockItem.blockText.h2;
+      blogText.appendChild(blogH2);
+
+    const blogP = document.createElement('p');
+      blogP.textContent = blockItem.blockText.p;
+      blogText.appendChild(blogP);
+
+  }
 }
-*/
-//JS
 
-function handleClick(dogName, breed, adoptFee) {
-  let result = null;
-  result = `You clicked on ${dogName}. They are a ${breed} and cost $${adoptFee}`;
-  alert(result);
-}
-
-
-
-//HTML
-/*
-<div class="box" onclick="handleClick('Murphy')">1</div>
-<div class="box" onclick="handleClick('Fido')">1</div>
-<div class="box" onclick="handleClick('Bullet')">1</div>
-*/
+// Manual way:
+          // const blogBlock = document.createElement('div');
+          //   blogBlock.setAttribute('class', 'blog-block');
+          //   blogContainer.appendChild(blogBlock);
+          //
+          // const blogImage = document.createElement('div');
+          //   blogImage.setAttribute('class', 'blog-image');
+          //   blogBlock.appendChild(blogImage);
+          //
+          // const blogRealImage = document.createElement('img');
+          //   blogRealImage.src = 'images/blog-1.jpg';
+          //   blogImage.appendChild(blogRealImage);
+          //
+          // const blogText = document.createElement('div');
+          //   blogText.setAttribute('class', 'blog-text');
+          //   blogBlock.appendChild(blogText);
+          //
+          // const blogH2 = document.createElement('h2');
+          //   blogH2.textContent = 'Traveling with your dog';
+          //   blogText.appendChild(blogH2);
+          //
+          // const blogP = document.createElement('p');
+          //   blogP.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+          //   blogText.appendChild(blogP);
