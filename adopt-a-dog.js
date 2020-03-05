5c748b8224e1eeea4af11a459d5f387e8ba762b0
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-03-04 22:43:15 -0800

    added more javascript on to the blog page. It's still a work in progress

--- adopt-a-dog.js
+++ adopt-a-dog.js
@@ -1,8 +1,37 @@
-const cart = [];
+// create a series of boxes, each box will have a headline
+// create a series of boxes, each box will have a headline
+const blogsArray = [
+  {
+    name: 'Adopt A Dog',
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
 
-const item = {name:"pet", cost:123.45, count:1};
+  },
+  {
+    name: 'Traveling With Your Dog',
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
 
-cart.push(item);
+  },
+  {
+    name: 'How to Walk Multiple Dogs',
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
 
-console.log(cart);
-console.log(cart[0].name);
+  },
+  {
+    name: 'Teach Your Dog To Fetch',
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
+  },
+  ];
+
+const target = document.getElementById(
+'container');
+
+
+blogsArray.forEach(function(blogs) {
+  const box = document.createElement('div');
+  box.setAttribute('class', 'box');
+  box.setAttribute('id', 'firstBox');
+  box.innerHTML += '<h1>' + blogs.name + '</h1>';
+  box.innerHTML += '<p>' + blogs.text + '</p>';
+
+  target.appendChild(box);
+})
