10cfc5d4d20683336197bc7d064d11f8f1511106
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-03-07 09:36:42 -0800

    updating homework with how I want the blogs to look like

--- adopt-a-dog.js
+++ adopt-a-dog.js
@@ -1,29 +1,24 @@
 // create a series of boxes, each box will have a headline
-// create a series of boxes, each box will have a headline
 const blogsArray = [
   {
-    name: 'Adopt A Dog',
-    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
-
-  },
-  {
     name: 'Traveling With Your Dog',
-    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
-
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    imageLink: 'https://placehold.co/600x400/EEE/31343C'
   },
-  {
+    {
     name: 'How to Walk Multiple Dogs',
-    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
-
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    imageLink: 'https://placehold.co/600x400/EEE/31343C'
   },
   {
     name: 'Teach Your Dog To Fetch',
-    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga.',
-  },
-  ];
+    text: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    imageLink: 'https://placehold.co/600x400/EEE/31343C'
+  }
+];
 
 const target = document.getElementById(
-'container');
+'blogcontainer');
 
 
 blogsArray.forEach(function(blogs) {
