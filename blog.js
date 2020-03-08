8f124798ffad7b2b0ce21670cc3b318ca1cb520a
Jenny Sugden <jensugden@gmail.com>
2020-03-08 13:59:38 -0700

    more work on appending elements via dom

---
+++ blog.js
@@ -0,0 +1,36 @@
+const blogArray = [
+
+  {
+    image: 'images/blog-1.jpg',
+    alt: 'Person hugging dog',
+    title: 'Traveling with Your Dog',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+
+  {
+    image: 'images/blog-2.jpg',
+    alt: 'Group of dogs on leashes',
+    title: 'How to Walk Multiple Dogs',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+
+  {
+    image: 'images/blog-3.jpg',
+    alt: 'Silhouette of person throwing a ball',
+    title: 'Teach Your Dog to Fetch!',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+];
+
+const target = document.getElementById('blog-container');
+
+blogArray.forEach(function(entry) {
+  const blog = document.createElement('div');
+  // blog.innerHTML += '<h2>' + entry.header + '</h2>';
+  blog.innerHTML += `<img src='${entry.image}'>`;
+  // blog.setAttribute('src', entry.alt);
+  blog.innerHTML += '<h3>' + entry.title + '</h3>';
+  blog.innerHTML += '<p>' + entry.text + '</p>';
+
+  target.appendChild(blog);
+});
--- purchase.js
+++ purchase.js
@@ -9,22 +9,120 @@ function dogTotal() {
   alert('$' + adoptFee);
 }
 
-const form = document.createElement('form');
-const fields = ['Name', 'Address'];
+// const formArray = [
+//     {
+//     name: 'Name',
+//     label: 'Name',
+//   },
+//
+//   {
+//     name: 'Address',
+//     label: 'Address',
+//   },
+//
+//   {
+//     name: 'City',
+//     label: 'City',
+//   },
+//
+//   {
+//     name: 'State',
+//     label: 'State',
+//   },
+//
+//   {
+//     name: 'Zip Code',
+//     label: 'Zip Code',
+//   },
+//
+//   {
+//     name: 'First Time',
+//     label: 'First Time Adopter?',
+//     input: 'radio',
+//   }
+// ];
+//
+// const checkout = document.getElementById('form');
+// const content = document.createElement('div');
+// const fields = ['Name', 'Address', 'City', 'State', 'Zip Code', 'First Time Adopter?'];
+//
+// for (let i = 0; i < fields.length; i += 1) {
+//   const label = document.createElement('label');
+//   label.textContent = fields[i];
+//   label.setAttribute('for', fields[i]);
+//
+//   const input = document.createElement('input');
+//   input.setAttribute('id', fields[i]);
+//
+//   form.appendChild(label);
+//   form.appendChild(input);
+// }
 
-for (let i = 0; i < fields.length; i += 1) {
-  const label = document.createElement('label');
-  label.textContent = fields[i];
-  label.setAttribute('for', fields[i]);
+const formSubmit = document.getElementById('form');
 
-  const input = document.createElement('input');
-  input.setAttribute('id', fields[i]);
+const usStates = [
+  { name: 'ALABAMA', abbreviation: 'AL'},
+  { name: 'ALASKA', abbreviation: 'AK'},
+  { name: 'ARIZONA', abbreviation: 'AZ'},
+  { name: 'ARKANSAS', abbreviation: 'AR'},
+  { name: 'CALIFORNIA', abbreviation: 'CA'},
+  { name: 'COLORADO', abbreviation: 'CO'},
+  { name: 'CONNECTICUT', abbreviation: 'CT'},
+  { name: 'DELAWARE', abbreviation: 'DE'},
+  { name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
+  { name: 'FLORIDA', abbreviation: 'FL'},
+  { name: 'GEORGIA', abbreviation: 'GA'},
+  { name: 'HAWAII', abbreviation: 'HI'},
+  { name: 'IDAHO', abbreviation: 'ID'},
+  { name: 'ILLINOIS', abbreviation: 'IL'},
+  { name: 'INDIANA', abbreviation: 'IN'},
+  { name: 'IOWA', abbreviation: 'IA'},
+  { name: 'KANSAS', abbreviation: 'KS'},
+  { name: 'KENTUCKY', abbreviation: 'KY'},
+  { name: 'LOUISIANA', abbreviation: 'LA'},
+  { name: 'MAINE', abbreviation: 'ME'},
+  { name: 'MARYLAND', abbreviation: 'MD'},
+  { name: 'MASSACHUSETTS', abbreviation: 'MA'},
+  { name: 'MICHIGAN', abbreviation: 'MI'},
+  { name: 'MINNESOTA', abbreviation: 'MN'},
+  { name: 'MISSISSIPPI', abbreviation: 'MS'},
+  { name: 'MISSOURI', abbreviation: 'MO'},
+  { name: 'MONTANA', abbreviation: 'MT'},
+  { name: 'NEBRASKA', abbreviation: 'NE'},
+  { name: 'NEVADA', abbreviation: 'NV'},
+  { name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
+  { name: 'NEW JERSEY', abbreviation: 'NJ'},
+  { name: 'NEW MEXICO', abbreviation: 'NM'},
+  { name: 'NEW YORK', abbreviation: 'NY'},
+  { name: 'NORTH CAROLINA', abbreviation: 'NC'},
+  { name: 'NORTH DAKOTA', abbreviation: 'ND'},
+  { name: 'OHIO', abbreviation: 'OH'},
+  { name: 'OKLAHOMA', abbreviation: 'OK'},
+  { name: 'OREGON', abbreviation: 'OR'},
+  { name: 'PENNSYLVANIA', abbreviation: 'PA'},
+  { name: 'RHODE ISLAND', abbreviation: 'RI'},
+  { name: 'SOUTH CAROLINA', abbreviation: 'SC'},
+  { name: 'SOUTH DAKOTA', abbreviation: 'SD'},
+  { name: 'TENNESSEE', abbreviation: 'TN'},
+  { name: 'TEXAS', abbreviation: 'TX'},
+  { name: 'UTAH', abbreviation: 'UT'},
+  { name: 'VERMONT', abbreviation: 'VT'},
+  { name: 'VIRGINIA', abbreviation: 'VA'},
+  { name: 'WASHINGTON', abbreviation: 'WA'},
+  { name: 'WEST VIRGINIA', abbreviation: 'WV'},
+  { name: 'WISCONSIN', abbreviation: 'WI'},
+  { name: 'WYOMING', abbreviation: 'WY' }
+];
 
-  form.appendChild(label);
-  form.appendChild(input);
+const stateSelect = document.getElementById('state');
+
+for (let i = 0; i < usStates.length; i++) {
+  const select = document.createElement('select');
+  select.text = usStates[i].name;
+  select.value = usStates[i].abbreviation
+  stateSelect.appendChild(select);
 }
 
-const formSubmit = document.getElementById('purchase');
 
 const submitButton = document.createElement('button');
 submitButton.setAttribute('type', 'submit');
@@ -36,4 +134,25 @@ submitButton.addEventListener('submit', function(event) {
   event.preventDefault();
   console.log(event.target.elements[0]);
   alert('form submitted');
+});
+
+document.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log(event);
+
+  // const formObject = {
+  //   name: 'Name',
+  //   emailAddress: 'Email Address',
+  //   address: 'Address',
+  //   city: 'City',
+  //   state: 'State',
+  //   zipCode: 'Zip Code',
+  //   first: 'Firt Time Adopter?',
+  //   pickup: 'Pickup Location',
+  // };
+  //
+  // for (let i = 0; i < event.target.elements.length - 1; i++) {
+  //   console.log(event.target.elements[i].value);
+  //   formObject[event.target.elements[i].id] = event.target.elements[i].value;
+  // }
 });
