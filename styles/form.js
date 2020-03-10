50094e694f01c497c98caad5fa17bcae0b5b1128
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-03-09 20:09:30 -0700

    finally hooked up javascript to my websites. It's now showing all my work

--- styles/form.js
+++ styles/form.js
@@ -1,38 +1,54 @@
-const field = [
-  {
-    name: 'full-name',
-    label: 'Full Name',
-  },
-  {
-    name: 'email',
-    label: 'Email Address',
-  },
+const fields = [
+    {
+        name: 'full-name',
+        label: 'Full Name'
+    },
+    {
+        name: 'email',
+        label: 'Email address'
+    },
+    {
+        name: 'phone',
+        label: 'Phone Number'
+    },
+    {
+        name: 'Address',
+        label: 'Street Address'
+    },
+    {
+        name: 'state',
+        label: 'State'
+    },
+    {
+        name: 'zip',
+        label: 'Zip Code'
+    },
 ];
 
 const form = document.createElement('form');
+
 document.body.appendChild(form);
 
-for (let i = 0; i < fields.length; i+= 1) {
-  const field =fields[i];
+for (let i = 0; i < fields.length; i += 1) {
+    const field = fields[i];
 
-  const label = document.createElement('label');
-  label.textContent = field.label;
-  label.setattribute('for', field.name);
+    const label = document.createElement('label');
+    label.setAttribute('for', field.name);
+    label.textContent = field.label;
 
-  const input = document.createElement('input');
-  input.setAttribute('id', filed.name);
+    const inputField = document.createElement('input');
+    inputField.setAttribute('id', field.name);
 
-  form.appendChild(label);
-  form.appendChild(input);
+    form.appendChild(label);
+    form.appendChild(inputField);
 }
 
 const submitButton = document.createElement('button');
 submitButton.setAttribute('type', 'submit');
 submitButton.textContent = 'Submit';
-
 form.appendChild(submitButton);
 
-form.addEvenListener('submit', function(event) {
-  event.preventDefault();
-  alert('Form submitted!');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!');
 });
