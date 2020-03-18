862cb132e902b0ee57cb691d003c0f3af6253ad1
kcbelford <kcbelford@hotmail.com>
2020-03-18 13:50:20 -0700

    some minor changes

--- scripts/lesson9.js
+++ scripts/lesson9.js
@@ -1,90 +1,87 @@
-$(document).ready(function() {
+// dog tile hover effect
 
-  // dog tile hover effect
-
-  $('.dog-img').hover(function(e) {
-    $(this).toggleClass('img-outline');
-  });
-
-
-  // update the checkout total on the index & dogs pages with each click of an adopt button (currently not working)
-
-  let balance = 0
-
-  $('#cart-total').text(balance);
-
-  $('.adopt').click(function(e) {
-    function dogFees(num) {
-    balance += num;
-    $('#cart-total').text(balance);
-    console.log('whaaat?');
-    }
-  });
-
-  // console logs form info (currently not working)
-
-  $('#checkout-form').submit(function(event) {
-
-    event.preventDefault();
-
-    const formObject = {};
-
-    for (let i = 0; i < event.target.elements.length - 1; i++) {
-
-      console.log(event.target.elements[i].id);
-
-      formObject[event.target.elements[i].id] = event.target.elements[i].value;
-    }
-
-    console.log(formObject);
-  });
-
-
-  // refactor the form submission
-
-  $('#checkout-wrapper').append('<h2 class="h2-padding">Checkout</h2>', '<div class="checkout-main"></div');
-
-  $('.checkout-main').append('<form id="checkout-form"></form>');
-
-  $('#checkout-form').append('<div class="checkout-content"></div>');
-
-  $('.checkout-content').append('<label for="name" class="input-label">Name</label>', '<input type="text" name="name" id="name">', '<label for="email" class="input-label">Email Address</label>', '<input type="email" name="email" id="email">', '<label for="address" class="input-label">Street Address</label>', '<input type="text" name="address" id="address">', '<div class="city-state-zip"></div>');
-
-  $('.city-state-zip').append('<div id="city-div"></div');
-
-  $('#city-div').append('<label for="city" class="cityzip input-label">City</label>', '<input class="city-input" type="text" name="city" id="city">');
-
-  $('.city-state-zip').append('<div id="state-div"></div');
-
-  $('#state-div').append('<label for="state" class="statezip input-label">State</label>')
-
-  $('.statezip').append('<select class="state-select" name="state" id="state"></select>');
-
-  $('.state-select').append('<option value="">-</option>', '<option value="alaska">Alaska</option>', '<option value="oregon">Oregon</option>', '<option value="washington">Washington</option>');
-
-  $('.city-state-zip').append('<div id="zip-div"></div>');
-
-  $('#zip-div').append('<label for="zipcode" class="zip input-label">Zip Code</label>', '<input class="zip-input" type="number" name="zipcode" id="zipcode">');
-
-  $('.checkout-content').append('<span class="input-label">First Time Adopter?</span>');
-
-  $('.checkout-content').append('<div class="radio-input"></div>');
-
-  $('.radio-input').append('<label class="no-label" for="no">No</label>', '<input class="no-input" type="radio" name="adopter" value="no" id="no">', '<label class="yes-label" for="yes">Yes</label>', '<input class="yes-input" type="radio" name="adopter" value="yes" id="yes">');
-
-  $('.checkout-content').append('<label for="pickup" class="input-label">Pickup Location</label>', '<select class="pickup-select" name="pickup" id="pickup"></select');
-
-  $('.pickup-select').append('<option value="">-</option>', '<option value="alaska">Alaska</option>', '<option value="oregon">Oregon</option>', '<option value="washington">Washington</option>');
-
-  $('#checkout-form').append('<section class="dogs-adopted-padding"></section');
-
-  $('.dogs-adopted-padding').append('<h3 class="input-label">Dogs Adopted</h3>', '<div class="dogs-adopted">');
-
-  $('.dogs-adopted').append('<div class="adopt-cost">');
-
-  $('.adopt-cost').append('<img class="adopt-img" src="images/lucas-checkout.jpg" alt="a black dog looking up at the camera">', '<div><h4>Lucas</h4><p>Cost to Adopt: $123.45</p></div>');
-
-  $('.adopt-cost').append('<img class="adopt-img" src="images/duffy-checkout.jpg" alt="a white and brown pitbull">', '<div><h4>Duffy</h4><p>Cost to Adopt: $123.45</p></div>');
-
-  $('#checkout-form').append('<section><h3 class="input-label">Total Adoption Cost</h3><p>$1234.56</p></section>', '<input onclick="submitForm()" class="submit" type="submit" value="Submit" name="submit">');
+$('.dog-img').hover(function(e) {
+  $(this).toggleClass('img-outline');
 });
+
+
+// update the checkout total on the index & dogs pages with each click of an adopt button (currently not working)
+
+let balance = 0
+
+$('#cart-total').text(balance);
+
+$('.adopt').click(function(e) {
+  function dogFees(num) {
+  balance += num;
+  $('#cart-total').text(balance);
+  console.log('whaaat?');
+  }
+});
+
+// console logs form info (currently not working)
+
+$('#checkout-form').submit(function(event) {
+
+  event.preventDefault();
+
+  const formObject = {};
+
+  for (let i = 0; i < event.target.elements.length - 1; i++) {
+
+    console.log(event.target.elements[i].id);
+
+    formObject[event.target.elements[i].id] = event.target.elements[i].value;
+  }
+
+  console.log(formObject);
+});
+
+
+// refactor the form submission
+
+$('#checkout-wrapper').append('<h2 class="h2-padding">Checkout</h2>', '<div class="checkout-main"></div');
+
+$('.checkout-main').append('<form id="checkout-form"></form>');
+
+$('#checkout-form').append('<div class="checkout-content"></div>');
+
+$('.checkout-content').append('<label for="name" class="input-label">Name</label>', '<input type="text" name="name" id="name">', '<label for="email" class="input-label">Email Address</label>', '<input type="email" name="email" id="email">', '<label for="address" class="input-label">Street Address</label>', '<input type="text" name="address" id="address">', '<div class="city-state-zip"></div>');
+
+$('.city-state-zip').append('<div id="city-div"></div');
+
+$('#city-div').append('<label for="city" class="cityzip input-label">City</label>', '<input class="city-input" type="text" name="city" id="city">');
+
+$('.city-state-zip').append('<div id="state-div"></div');
+
+$('#state-div').append('<label for="state" class="statezip input-label">State</label>')
+
+$('.statezip').append('<select class="state-select" name="state" id="state"></select>');
+
+$('.state-select').append('<option value="">-</option>', '<option value="alaska">Alaska</option>', '<option value="oregon">Oregon</option>', '<option value="washington">Washington</option>');
+
+$('.city-state-zip').append('<div id="zip-div"></div>');
+
+$('#zip-div').append('<label for="zipcode" class="zip input-label">Zip Code</label>', '<input class="zip-input" type="number" name="zipcode" id="zipcode">');
+
+$('.checkout-content').append('<span class="input-label">First Time Adopter?</span>');
+
+$('.checkout-content').append('<div class="radio-input"></div>');
+
+$('.radio-input').append('<label class="no-label" for="no">No</label>', '<input class="no-input" type="radio" name="adopter" value="no" id="no">', '<label class="yes-label" for="yes">Yes</label>', '<input class="yes-input" type="radio" name="adopter" value="yes" id="yes">');
+
+$('.checkout-content').append('<label for="pickup" class="input-label">Pickup Location</label>', '<select class="pickup-select" name="pickup" id="pickup"></select');
+
+$('.pickup-select').append('<option value="">-</option>', '<option value="alaska">Alaska</option>', '<option value="oregon">Oregon</option>', '<option value="washington">Washington</option>');
+
+$('#checkout-form').append('<section class="dogs-adopted-padding"></section');
+
+$('.dogs-adopted-padding').append('<h3 class="input-label">Dogs Adopted</h3>', '<div class="dogs-adopted">');
+
+$('.dogs-adopted').append('<div class="adopt-cost">');
+
+$('.adopt-cost').append('<img class="adopt-img" src="images/lucas-checkout.jpg" alt="a black dog looking up at the camera">', '<div><h4>Lucas</h4><p>Cost to Adopt: $123.45</p></div>');
+
+$('.adopt-cost').append('<img class="adopt-img" src="images/duffy-checkout.jpg" alt="a white and brown pitbull">', '<div><h4>Duffy</h4><p>Cost to Adopt: $123.45</p></div>');
+
+$('#checkout-form').append('<section><h3 class="input-label">Total Adoption Cost</h3><p>$1234.56</p></section>', '<input onclick="submitForm()" class="submit" type="submit" value="Submit" name="submit">');
--- scripts/script.js
+++ scripts/script.js
@@ -1,104 +1,101 @@
-window.onload = function() {
+//click dog pictures - alerts dog info
 
-  //click dog pictures - alerts dog info
+function dogInfo(nameFees) {
+  alert(nameFees);
+}
 
-  function dogInfo(nameFees) {
-    alert(nameFees);
+
+// click adopt button - alerts total cost
+
+let totalCost = 0;
+
+function dogFees(num) {
+  totalCost += num;
+  alert('Total Fees: ' + '$' + totalCost / 100);
+}
+
+
+// alerts user form has been submitted
+
+function submitForm() {
+  alert('The form information has been received. There is no turning back now.');
+}
+
+// blog page content
+
+const blogContainer = document.getElementById('blog-container');
+const blogArr = [
+  {
+    imageLink: './images/blog-1.jpg',
+    altTag: 'a person sitting with their arm around their dog looking out at a canyon',
+    headline: 'Traveling With Your Dog',
+    textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
+    textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
+  },
+  {
+    imageLink: './images/blog-2.jpg',
+    altTag: 'four different dogs being walted by one person',
+    headline: 'How To Walk Multiple Dogs',
+    textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
+    textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
+  },
+  {
+    imageLink: './images/blog-3.jpg',
+    altTag: 'a silhouette of a dog and a person who is holding up a ball',
+    headline: 'Teach Your Dog To Fetch!',
+    textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
+    textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
   }
+]
 
+for (let i = 0; i < blogArr.length; i++) {
 
-  // click adopt button - alerts total cost
+  const blogArticle = document.createElement('article');
+  blogArticle.setAttribute('class', 'blog-post');
 
-  let totalCost = 0;
+  const image = document.createElement('img');
+  image.setAttribute('class', 'blog-img');
+  image.setAttribute('src', blogArr[i].imageLink);
+  image.setAttribute('alt', blogArr[i].altTag);
+  blogArticle.appendChild(image);
 
-  function dogFees(num) {
-    totalCost += num;
-    alert('Total Fees: ' + '$' + totalCost / 100);
-  }
+  const paraDiv = document.createElement('div');
+  paraDiv.setAttribute('class', 'blog-para');
+  blogArticle.appendChild(paraDiv);
 
+  const blogHeader = document.createElement('h3');
+  blogHeader.innerText = blogArr[i].headline;
+  paraDiv.appendChild(blogHeader);
 
-  // alerts user form has been submitted
+  const blogParaOne = document.createElement('p');
+  blogParaOne.innerText = blogArr[i].textOne;
+  blogParaOne.setAttribute('class', 'blog-post-font');
+  paraDiv.appendChild(blogParaOne);
 
-  function submitForm() {
-    alert('The form information has been received. There is no turning back now.');
-  }
+  const blogParaTwo = document.createElement('p');
+  blogParaTwo.innerText = blogArr[i].textTwo;
+  blogParaTwo.setAttribute('class', 'blog-post-font');
+  paraDiv.appendChild(blogParaTwo);
 
-  // blog page content
+  blogContainer.appendChild(blogArticle);
+}
 
-  const blogContainer = document.getElementById('blog-container');
-  const blogArr = [
-    {
-      imageLink: './images/blog-1.jpg',
-      altTag: 'a person sitting with their arm around their dog looking out at a canyon',
-      headline: 'Traveling With Your Dog',
-      textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
-      textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
-    },
-    {
-      imageLink: './images/blog-2.jpg',
-      altTag: 'four different dogs being walted by one person',
-      headline: 'How To Walk Multiple Dogs',
-      textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
-      textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
-    },
-    {
-      imageLink: './images/blog-3.jpg',
-      altTag: 'a silhouette of a dog and a person who is holding up a ball',
-      headline: 'Teach Your Dog To Fetch!',
-      textOne: 'Doggo ipsum blop tungg woofer borkdrive tungg, doggorino stop it fren puggorino stop it fren h*ck, much ruin diet maximum borkdrive wow very biscit. Blep heckin angery woofer he made many woofs blep, smol borking doggo with a long snoot for pats doing me a frighten. Heck sub woofer dat tungg tho what a nice floof you are doing me a frighten fat boi, very good spot boofers aqua doggo.',
-      textTwo: 'Long doggo the neighborhood pupper most angery pupper I have ever seen ur givin me a spook pupperino, pats borkdrive yapper. Such treat stop it fren yapper long water shoob much ruin diet, pupper borkdrive. Wow very biscit pupperino doggorino fat boi I am bekom fat boof puggo I am bekom fat extremely cuuuuuute, borking doggo I am bekom fat boof doggo tungg heckin good boys.'
-    }
-  ]
-
-  for (let i = 0; i < blogArr.length; i++) {
-
-    const blogArticle = document.createElement('article');
-    blogArticle.setAttribute('class', 'blog-post');
-
-    const image = document.createElement('img');
-    image.setAttribute('class', 'blog-img');
-    image.setAttribute('src', blogArr[i].imageLink);
-    image.setAttribute('alt', blogArr[i].altTag);
-    blogArticle.appendChild(image);
-
-    const paraDiv = document.createElement('div');
-    paraDiv.setAttribute('class', 'blog-para');
-    blogArticle.appendChild(paraDiv);
-
-    const blogHeader = document.createElement('h3');
-    blogHeader.innerText = blogArr[i].headline;
-    paraDiv.appendChild(blogHeader);
-
-    const blogParaOne = document.createElement('p');
-    blogParaOne.innerText = blogArr[i].textOne;
-    blogParaOne.setAttribute('class', 'blog-post-font');
-    paraDiv.appendChild(blogParaOne);
-
-    const blogParaTwo = document.createElement('p');
-    blogParaTwo.innerText = blogArr[i].textTwo;
-    blogParaTwo.setAttribute('class', 'blog-post-font');
-    paraDiv.appendChild(blogParaTwo);
-
-    blogContainer.appendChild(blogArticle);
-  }
-
-  // console logs form inputs (currently not working)
-  //
-  // const getForm = document.getElementById('checkout-form');
-  //
-  // getForm.addEventListener('submit', function(event) {
-  //
-  //   event.preventDefault();
-  //
-  //   const formObject = {};
-  //
-  //   for (let i = 0; i < event.target.elements.length - 1; i++) {
-  //
-  //     console.log(event.target.elements[i].id);
-  //
-  //     formObject[event.target.elements[i].id] = event.target.elements[i].value;
-  //   }
-  //
-  //   console.log(formObject);
-  // });
-};
+// console logs form inputs (currently not working)
+//
+// const getForm = document.getElementById('checkout-form');
+//
+// getForm.addEventListener('submit', function(event) {
+//
+//   event.preventDefault();
+//
+//   const formObject = {};
+//
+//   for (let i = 0; i < event.target.elements.length - 1; i++) {
+//
+//     console.log(event.target.elements[i].id);
+//
+//     formObject[event.target.elements[i].id] = event.target.elements[i].value;
+//   }
+//
+//   console.log(formObject);
+// });
